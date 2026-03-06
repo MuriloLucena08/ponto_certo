3a4d29819993d995f35b0fc78b8a08f636180111
Murilo Carvalho de Lucena <01009581245@semob.df.gov.br>
2026-03-06 11:57:10 -0300

    subindo novamente

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   preview: {
     proxy: {
       '/api-semob': {
-        target: 'https://2d3b-131-72-222-138.ngrok-free.app',
+        target: 'https://2d3b-131-72-222-138.ngrok-free.app/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api-semob/, '')
@@ -81,8 +81,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api-semob': {
-        target: 'https://2d3b-131-72-222-138.ngrok-free.app',
-        
+        target: 'https://2d3b-131-72-222-138.ngrok-free.app/',
+
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api-semob/, '')
